fa383ce42c8b784d6b7150d2f97b62dd70e37af4
YeeunGo <yeeun2993@naver.com>
2023-12-18 12:32:26 +0900

    commit

---
+++ AssetEdu/src/main/java/kr/co/kfs/assetedu/model/Student.java
@@ -0,0 +1,17 @@
+package kr.co.kfs.assetedu.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private String id;
+	private Integer age;
+	private Double weight;
+	private Long asset;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate birth;
+}
